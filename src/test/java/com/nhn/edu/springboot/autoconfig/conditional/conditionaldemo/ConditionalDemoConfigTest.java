ad8d4d781875faa9deee924a039d0df08b41bd90
정지범 <jibum.jung@nhnent.com>
2019-04-10 17:13:11 +0900

    fix test case for sayYesComponentWeb, sayYesComponentNotWeb

--- src/test/java/com/nhn/edu/springboot/autoconfig/conditional/conditionaldemo/ConditionalDemoConfigTest.java
+++ src/test/java/com/nhn/edu/springboot/autoconfig/conditional/conditionaldemo/ConditionalDemoConfigTest.java
@@ -1,20 +1,34 @@
 package com.nhn.edu.springboot.autoconfig.conditional.conditionaldemo;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 
 /**
  * 아래의 모든 테스트 케이스가 성공할 수 있도록 프로젝트를 수정해 주세요.
  * 下記のすべてのテストケースが成功できるようにプロジェクトを修正してください。
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ConditionalDemoConfigTest {
 
-    private final ApplicationContextRunner contextRunner 
+    private final ApplicationContextRunner contextRunner
         = new ApplicationContextRunner()
         .withConfiguration(AutoConfigurations.of(ConditionalDemoConfiguration.class));
+
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     /**
      * 이 테스트를 성공하려면 TODO(1) 를 수정하세요.
@@ -33,8 +47,8 @@ public class ConditionalDemoConfigTest {
      */
     @Test
     public void sayYesComponentWeb() {
-        this.contextRunner
-            .run(context -> assertThat(context).hasBean("sayYesComponentWeb"));
+        assertTrue(this.applicationContext.containsBean("sayYesComponentWeb"));
+        assertFalse(this.applicationContext.containsBean("sayYesComponentNotWeb"));
     }
 
 
